244751b22771ddacf4f777552957c5de44a75667
Divine Orji <dpkreativ@gmail.com>
2021-12-05 06:54:52 +0100

    test other users

--- scripts/run.js
+++ scripts/run.js
@@ -18,6 +18,11 @@ const main = async () => {
   await waveTxn.wait();
 
   waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
